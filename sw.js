a398843d9863ad71b0046737d4f679afdd218f7b
sharifmb92-bit <270324566+sharifmb92-bit@users.noreply.github.com>
2026-09-07 07:40:23 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,36 @@
+// medbasha
+const CACHE_NAME = 'bustalk-v298-gps-v1';
+const assetsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assetsToCache))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+      return fetch(event.request).catch(() => caches.match('./index.html'));
+    })
+  );
+});
